4e5282416428548ef2db0e9123c3474a325d9383
afransami <afran.sami007@gmail.com>
2023-05-08 00:28:39 +0600

    serverside data added

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 // const uri = "mongodb+srv://coffeeMaster:K7K3oYaBvfPWupZf@cluster0.m7jzpqk.mongodb.net/?retryWrites=true&w=majority";
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m7jzpqk.mongodb.net/?retryWrites=true&w=majority`;
-console.log(process.env.DB_USER);
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -35,11 +35,22 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const coffeeCollection = client.db ('coffeeDB').collection('coffee')
+
+    app.post ('/coffee', async(req, res) =>{
+        const newCoffee = req.body
+        console.log(newCoffee);
+        const result = await coffeeCollection.insertOne(newCoffee);
+        res.send (result)
+    })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
